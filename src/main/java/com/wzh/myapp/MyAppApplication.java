619ae4e65a0521c6a8923fdb28b5d50a8c2ecd50
WangsSmall <15093846650@163.com>
2024-05-08 11:17:50 +0800

    根据提供的diff信息，可以看出删除了BasicController.java、PathVariableController.java、User.java和index.html文件，以及修改了MyAppApplication.java文件。因此，commit message可以写为：
    删除基本控制器、路径变量控制器、用户模型以及静态资源文件，并更新应用启动类。

--- src/main/java/com/wzh/myapp/MyAppApplication.java
+++ src/main/java/com/wzh/myapp/MyAppApplication.java
@@ -3,6 +3,9 @@ package com.wzh.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 75654
+ */
 @SpringBootApplication
 public class MyAppApplication {
 
